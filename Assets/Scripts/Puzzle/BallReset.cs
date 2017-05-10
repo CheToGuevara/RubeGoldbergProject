75eba50f236303f05879c712dd6f20cbb97c9cb6
Aaron Sujar <Aaron Sujar>
2017-05-10 17:40:18 +0100

    feat: objects create

---
+++ Assets/Scripts/Puzzle/BallReset.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallReset : MonoBehaviour {
+
+
+    public Rigidbody m_rigidbody;
+    // Use this for initialization
+    Vector3 startposition;
+    Vector3 zero = new Vector3(0f, 0f, 0f);
+    
+	void Start () {
+        startposition = transform.position;
+	}
+
+    // Update is called once per frame
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            transform.position = startposition;
+            m_rigidbody.velocity = zero;
+            m_rigidbody.angularVelocity = zero;
+            transform.rotation = Quaternion.Euler(zero);
+        }
+            
+    }
+}
---
+++ Assets/Scripts/Puzzle/React2Ball.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class React2Ball : MonoBehaviour {
+
+    public bool iamafan = false;
+
+    private Transform m_blades;
+	// Use this for initialization
+	void Start () {
+		if (iamafan)
+        {
+            m_blades = transform.GetChild(0);
+            InvokeRepeating("onRotation", 0.5f, 0.1f);
+        }
+	}
+
+
+    private void onRotation()
+    {
+        m_blades.Rotate(transform.forward*Time.deltaTime*1000);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.transform.CompareTag("Throwable"))
+        {
+            Debug.Log(collision.relativeVelocity  * 100  );
+            collision.gameObject.GetComponent<Rigidbody>().AddForce( collision.relativeVelocity.x * 50, -collision.relativeVelocity.y * 50, collision.relativeVelocity.z * 50);
+        }
+            
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (transform.CompareTag("Throwable"))
+        {
+            //Debug.Log();
+            other.gameObject.GetComponent<Rigidbody>().AddForce(transform.up);
+        }
+        else if(transform.CompareTag("Collectable"))
+                {
+            
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.transform.CompareTag("Throwable"))
+        {
+            Debug.Log("Esto");
+            other.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward*10);
+        }
+    }
+    
+}
