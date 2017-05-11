e0cc15598afff161918ad1c935d2b64c82680e0c
Aaron Sujar <Aaron Sujar>
2017-05-11 23:05:05 +0100

    feat: object teleport done

---
+++ Assets/Scripts/Puzzle/TeleportManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportManager : MonoBehaviour {
+
+    public TeleportTrigger[] m_triggers;
+	// Use this for initialization
+	void Start () {
+        for(int i=0; i < m_triggers.Length;i++)
+        {
+            m_triggers[i].teleporternumber = i;
+        }
+	}
+	
+	public void detectBallinTeleport(int numofteleport,GameObject ball)
+    {
+        int numoft = ((numofteleport + 1) % m_triggers.Length);
+        Transform otherteleport =  m_triggers[numoft].transform;
+        m_triggers[numoft].DeActivateCollider();
+        Debug.Log(otherteleport.up * 10);
+        ball.transform.position = otherteleport.position + otherteleport.forward*0.5f;
+
+
+    }
+}
---
+++ Assets/Scripts/Puzzle/TeleportTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportTrigger : MonoBehaviour
+{
+
+    public TeleportManager teleportmanager;
+
+    public int teleporternumber;
+
+    public Collider myownCollider;
+    // Use this for initialization
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        teleportmanager.detectBallinTeleport(teleporternumber, other.gameObject);
+        DeActivateCollider();
+    }
+
+    private void ReEnableCollider()
+    {
+        myownCollider.enabled = true;
+    }
+
+    public void DeActivateCollider()
+    {
+        myownCollider.enabled = false;
+        Invoke("ReEnableCollider", 1);
+    }
+
+}
